bc8268e3377db0216b41c5afe5e01943bd1eb092
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-06 13:44:46 +0300

    Showing data

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 
-const EmployeeList = () => {
+import EmployeeDetail from './employee_detail';
+
+const EmployeeList = (props) => {
+
   return (
     <div>
       <div className="employee-list">
-        Employee List
+        {props.employees.map(employee => <EmployeeDetail employee={employee} key={employee._id}/>)}
       </div>
     </div>
   );
