9cf8e70453624aba531d4a098d91494126ac13a8
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-06 14:35:40 +0300

    Load more functionality

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 
 import EmployeeDetail from './employee_detail';
 
-const EmployeeList = (props) => {
+const PER_PAGE = 20;
 
+class EmployeeList extends Component {
+  componentWillMount(){
+    this.page = 1;
+  }
+  handleButtonClick() {
+     Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+     this.page +=1;
+  }
+  render() {
   return (
     <div>
       <div className="employee-list">
-        {props.employees.map(employee => <EmployeeDetail employee={employee} key={employee._id}/>)}
+        {this.props.employees.map(employee => <EmployeeDetail employee={employee} key={employee._id}/>)}
       </div>
+      <button onClick={this.handleButtonClick.bind(this)}
+        className="btn btn-primary">Load More...</button>
     </div>
   );
+  }
 };
 
 export default createContainer(() => {
   //set up subscription
-  Meteor.subscribe('employees');
+  Meteor.subscribe('employees', PER_PAGE);
 
   return { employees: Employees.find({}).fetch() }
 }, EmployeeList);
